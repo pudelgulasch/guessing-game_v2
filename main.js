749fc756aa7e1b7de857bd4710682810ac9267cc
pudelgulasch <katrinsalac@gmail.com>
2019-08-18 10:06:08 +0200

    add

---
+++ main.js
@@ -0,0 +1,15 @@
+let numbers= document.querySelector("div");
+let guessesleft=document.querySelector("#guesses-left");
+let random=Math.floor(Math.random()*10+1);
+
+
+//  console.log(numbers[0].innerText);
+
+
+
+document.addEventListener("click",e=>{
+    console.log(e.target);   
+// if (random===e.target.innerText){
+// }
+})
+
