d754d005ccf4ce61b95ad74d3913a84e2c3e7091
pudelgulasch <katrinsalac@gmail.com>
2019-08-19 10:25:48 +0200

    add

--- mycode/main.js
+++ mycode/main.js
@@ -1,40 +1,52 @@
-let numbers= document.querySelectorAll("div a");
-let h2=document.querySelector("h2");
-let guessesleft=document.querySelector("#guesses-left");
-let popup=document.querySelector(".popup");
-let clicks=0;
-let random=Math.floor(Math.random()*10+1);
+let numbers = document.querySelectorAll("div a");
+let h2 = document.querySelector("h2");
+let guessesleft = document.querySelector("#guesses-left");
+let popup = document.querySelector(".popup");
+let clicks = 0;
+let random = Math.floor(Math.random() * 10 + 1);
 
 window.onload;
 
-for (let i=0;i<numbers.length;i++){
-    numbers[i].addEventListener("click",numberEqual)
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", numberEqual);
 }
 
 function numberEqual(e) {
-    if(clicks<2 && random != e.target.innerText){
-        clicks++;
-        guessesleft.innerHTML=3-clicks;
-        e.preventDefault();
-        if(clicks == 2 && random != e.target.innerText){
-            h2.innerHTML="One more try!";   }}
-            else if (random == e.target.innerText){
-                h2.innerText="...";
-                popup.style.display="block";
-                popup.innerHTML="You won! One more time?"+popup.innerHTML;
-                numbers.innerHTML=popup; 
-                e.preventDefault();
-            };  
-            console.log(e.target.innerText);
-            console.log(random);
-            console.log(clicks);
-        };
-          
-    popup.addEventListener("click",e=>{
-        if (e.target.id=="no"){
-            window.close();
-        } else if (e.target.id=="yes"){
-            document.location.reload(true);
-        }
-    });
+  if (clicks < 2 && random != e.target.innerText) {
+    clicks++;
+    guessesleft.innerHTML = 3 - clicks;
+    e.preventDefault();
+    if (clicks == 2 && random != e.target.innerText) {
+      h2.innerHTML = "One more try!";
+    }
+  } else if (random == e.target.innerText) {
+    h2.innerText = "...";
+    popup.style.display = "block";
+    popup.innerHTML = "You won! One more time?" + popup.innerHTML;
+    for (let i = 0; i < numbers.length; i++) {
+      numbers[i].removeEventListener("click", numberEqual);
+      numbers[i].removeAttribute("href");
+    }
+    e.preventDefault();
+  } else if (clicks <= 3 && random != e.target.innerText) {
+    popup.style.display = "block";
+    popup.innerHTML = "You lost. You should try again!" + popup.innerHTML;
+    for (let i = 0; i < numbers.length; i++) {
+      numbers[i].removeEventListener("click", numberEqual);
+      numbers[i].removeAttribute("href");
+    }
+    e.preventDefault();
+  }
 
+  console.log(e.target.innerText);
+  console.log(random);
+  console.log(clicks);
+}
+
+popup.addEventListener("click", e => {
+  if (e.target.id == "no") {
+    window.close();
+  } else if (e.target.id == "yes") {
+    document.location.reload(true);
+  }
+});
